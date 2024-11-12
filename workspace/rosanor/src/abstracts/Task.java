fad004522f2aac779fbfaf27b8031c85648399fa
Nicolas Picaud <nicolas.picaud@etu.univ-savoie.fr>
2024-11-12 18:21:54 +0100

    Removed Thread from Task ^^'

--- workspace/rosanor/src/abstracts/Task.java
+++ workspace/rosanor/src/abstracts/Task.java
@@ -1,6 +1,6 @@
 package abstracts;
 
-public abstract class Task extends Thread {
+public abstract class Task {
 	public abstract void post(Runnable r, String name);
 	public abstract void kill();
 	public abstract boolean killed();
